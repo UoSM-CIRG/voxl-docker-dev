e165a504850657331b0536ed9992c7e23177f145
UOSM-CIRG-2023-UAV95 <50212147+limshoonkit@users.noreply.github.com>
2023-11-21 14:14:18 +0800

    add square pattern maneuver and gradual descent

--- mavros_test/catkin_ws/src/offboard_test/src/offboard_test_node.cpp
+++ mavros_test/catkin_ws/src/offboard_test/src/offboard_test_node.cpp
@@ -6,12 +6,14 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 
 mavros_msgs::State current_state;
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
+void state_cb(const mavros_msgs::State::ConstPtr &msg)
+{
     current_state = *msg;
 }
 
@@ -20,20 +22,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_position/local", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
-    //the setpoint publishing rate MUST be faster than 2Hz
+    // the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
     // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
+    while (ros::ok() && !current_state.connected)
+    {
         ros::spinOnce();
         rate.sleep();
     }
@@ -42,9 +41,11 @@ int main(int argc, char **argv)
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = 1.5;
+    pose.pose.orientation = tf2::toMsg(tf2::Quaternion(0, 0, 0, 1)); // No rotation (yaw = 0)
 
-    //send a few setpoints before starting
-    for(int i = 100; ros::ok() && i > 0; --i){
+    // send a few setpoints before starting
+    for (int i = 100; ros::ok() && i > 0; --i)
+    {
         local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
@@ -58,27 +59,68 @@ int main(int argc, char **argv)
 
     ros::Time last_request = ros::Time::now();
 
-    while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
+    while (ros::ok() && pose.pose.position.z > 0.5)
+    {
+        if (current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0)))
+        {
+            if (set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent)
+            {
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
+        }
+        else
+        {
+            if (!current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0)))
+            {
+                if (arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success)
+                {
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
             }
         }
-
+        pose.pose.position.z -= 0.01;
         local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
 
+        // Move to x=1, y=0
+        pose.pose.position.x = 0.3;
+        pose.pose.position.y = 0.0;
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+
+        // Move to x=1, y=1
+        pose.pose.position.x = 0.3;
+        pose.pose.position.y = 0.3;
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+
+        // Move to x=-1, y=1
+        pose.pose.position.x = -0.3;
+        pose.pose.position.y = 0.3;
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+
+        // Move to x=-1, y=-1
+        pose.pose.position.x = -0.3;
+        pose.pose.position.y = -0.3;
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+
+        // Move to x=1, y=-1
+        pose.pose.position.x = 0.3;
+        pose.pose.position.y = -0.3;
+        local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
     }
