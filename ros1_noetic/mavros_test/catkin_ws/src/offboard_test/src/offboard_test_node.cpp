33ea8d8ba35cee99d6c3162331568b985fdaa654
nvidia <50212147+limshoonkit@users.noreply.github.com>
2024-03-17 22:24:16 +0800

    update dockerfile for nav_msgs dependency

--- ros1_noetic/mavros_test/catkin_ws/src/offboard_test/src/offboard_test_node.cpp
+++ ros1_noetic/mavros_test/catkin_ws/src/offboard_test/src/offboard_test_node.cpp
@@ -7,7 +7,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <nav_msgs/msg/odometry.hpp>
+#include <nav_msgs/msg/odometry.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
