9bdcffbcc6103874aa323da4c928af18c424bbb0
limshoonkit <50212147+limshoonkit@users.noreply.github.com>
2024-08-13 16:55:58 +0800

    update launch script

--- ros1_noetic/mavros_test/run_offboard_test.sh
+++ ros1_noetic/mavros_test/run_offboard_test.sh
@@ -4,4 +4,18 @@
 
 
 source ros_environment.sh
-roslaunch offboard_test offboard_test.launch flight_pattern:=1 flight_height:=1.00
+
+# flight_pattern - 0 = Circular, 1 = Spiral, 2 = Cloud, 3 = Sine, 4 = N-gram
+# max_iter - 2
+# dt - 0.05
+# radius - 1.00 m
+# height - 1.00 m
+# speed 
+# min_speed
+# offset_x - 0.00 m
+# offset_y - 0.00 m
+# offset_z - 0.50 m
+# frequency
+# ngram_vertices
+# ngram_step
+roslaunch offboard_test offboard_test.launch flight_pattern:=0 height:=1.00
